e93afc99ee30ef69e3e09e906a1cb1067a9e8bf2
João vitor de Oliveira Ferreira <89479485+Joao-vitor-090@users.noreply.github.com>
2023-05-17 16:32:43 -0300

    ++ Retorno da aplicação - QuicSort
    => Foi ajeitado as '{'.
    ++ Adicionado retorno da aplicação, onde foi usado recursividade os operadores de propagação (speed operator).
    ++Adicionado Campo de teste.

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
-const QuickSort = (array) => { array.length <= 1 ? array : 0 }  //Caso o array tenha apenas uma casa ou menos ele já está ordenado
+const quickSort = (array) => {
+    array.length <= 1 ? array : 0  //Caso o array tenha apenas uma casa ou menos ele já está ordenado
 
-const array = [];
-const pivo  = array[Math.floor(array.length / 2)];  // seleciona o array do meio
-const left  = [];                                   //elemento à esquerda  
-const right = [];                                   //Elemento à direita
+    const array = [];
+    const pivo = array[Math.floor(array.length / 2)];  // seleciona o array do meio
+    const left = [];                                   //elemento à esquerda  
+    const right = [];                                   //Elemento à direita
 
-for (let i = 0; i < array.length; i++) {
-    
-    array[i] > pivo ? left.push(array[i]): 0;
+    for (let i = 0; i < array.length; i++) {
 
-    array[i] < pivo ? right.push(array[i]) : 0;
+        array[i] > pivo ? left.push(array[i]) : 0;
+
+        array[i] < pivo ? right.push(array[i]) : 0;
+    }
+
+    return [...quickSort(left), pivot, ...quickSort(right)];
 }
+
+
+                                                     /*TESTE APLICAÇÃO*/
